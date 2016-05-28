88314540e05f546b04c3d7498864eba93da538da
Takatoshi Matsumoto <toqoz403@gmail.com>
2016-05-28 12:54:56 +0900

    fix(api-gateway-localdev): fix sorting routes

--- index.js
+++ index.js
@@ -24,11 +24,27 @@ module.exports = function(app, routes) {
   paramRegexp = /^{[a-zA-Z0-9._-]+}$/
   normalRegexp = /^[a-zA-Z0-9._-]+$/
 
+  var maxDepth = 0;
+  routes.forEach(function(r) {
+    var d = r.path.split('/').length
+    if (d > maxDepth) {
+      maxDepth = d;
+    }
+  });
   routes.sort(function(a, b) {
     var al = a.path.length;
-    if (a.path.indexOf("{") !== -1) al += 999;
+    a.path.split("/").forEach(function(segment, i) {
+      if (segment.indexOf("{") !== -1) {
+        al += 999 * (maxDepth - i);
+      }
+    });
+
     var bl = b.path.length;
-    if (b.path.indexOf("{") !== -1) bl += 999;
+    b.path.split("/").forEach(function(segment, i) {
+      if (segment.indexOf("{") !== -1) {
+        bl += 999 * (maxDepth - i);
+      }
+    });
 
     return al - bl;
   });
